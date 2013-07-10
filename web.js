21a5b0442d429f4443751c9434d65bc80de54d93
Kiran Satti <kiran.satti@gmailcom>
2013-07-09 18:59:23 -0700

    Updated index.html

--- web.js
+++ web.js
@@ -1,9 +1,19 @@
+//var express = require('express');
+//var fs = require('fs');
+//var testdata = fs.readFileSync('Test.txt');
+//console.log(testdata);
+//var buf = new Buffer('test');
+//var buf1 = buf.toString();
+//console.log(buf1);
+//console.log(buf);
+
 var express = require('express');
-
+var fs = require('fs');
+var indexfilename = "index.html";
 var app = express.createServer(express.logger());
-
+var filedata = new Buffer(fs.readFileSync(indexfilename));
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(filedata.toString());
 });
 
 var port = process.env.PORT || 5000;
